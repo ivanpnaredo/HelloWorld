b6e1c27b18e8ad04b9fd4a6fbddafc061a1d4901
Iván Peláez Naredo <ivanpnaredo@gmail.com>
2018-02-02 18:10:42 +0100

    Adding the files

---
+++ app/src/main/java/com/example/ivs_n/helloworld/HelloWorld2Activity.kt
@@ -0,0 +1,12 @@
+package com.example.ivs_n.helloworld
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class HelloWorld2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hello_world2)
+    }
+}
